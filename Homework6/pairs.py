4433d5fa07ab7ed3806601ef7da54f7359fde4ae
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2018-12-16 14:48:45 -0500

    intermediate - 2

--- Homework6/pairs.py
+++ Homework6/pairs.py
@@ -5,13 +5,12 @@ sc = SparkContext("local", "app")
 
 graph_file = sc.textFile(sys.argv[1])
 
-counts = graph_file.map(lambda line: (line.split('\t')[0],line.split('\t')[1]))
+counts = graph_file.map(lambda line: (line.split('\t')[0],line.split('\t')[1]))\
+.groupByKey(numPartitions=None)\
+.mapValues(list)\
+.sortByKey(ascending=True)
+
 counts_1 = graph_file.map(lambda line: (line.split('\t')[1],line.split('\t')[0]))
-#                   .map(lambda degree: (degree.split('\t')[0],int(degree.split('\t')[1]))) \
-#                   .reduceByKey(lambda a,b: a+b) \
-#                   .sortByKey(ascending=True)
 
-counts.reduceByKey(lambda a,b:a+1)
 
-#counts.saveAsTextFile(sys.argv[2])
 counts.saveAsTextFile(sys.argv[2])

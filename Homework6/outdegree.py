5b31f96ad8e49de6e994cb372a2750267eebcc8f
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2018-12-15 22:03:41 -0500

    final commit for outdegree

--- Homework6/outdegree.py
+++ Homework6/outdegree.py
@@ -1,28 +1,13 @@
 from pyspark import SparkContext
 import sys
-import csv
-import os 
 
 sc = SparkContext("local", "app")
 
-if os.path.exists("temp.txt"):
-  os.remove("temp.txt")
-file = open("temp.txt", "w") 
+graph_file = sc.textFile(sys.argv[1])
 
-with open(sys.argv[1]) as tsvfile:
-    tsvreader = csv.reader(tsvfile,delimiter ="\t")
-    for line in tsvreader:
-        file.write(line[0]+' ')
-    file.close()
-
-graph_file = sc.textFile("temp.txt")
-
-
-
-counts = graph_file.flatMap(lambda line: line.split(" "))\
-    .map(lambda degree: (degree,1)) \
-    .reduceByKey(lambda a,b: a+b)
+counts = graph_file.map(lambda line: line.split('\t')[0]) \
+                   .map(lambda degree: (degree,1)) \
+                   .reduceByKey(lambda a,b: a+b) \
+                   .sortByKey(ascending=True)
 
 counts.saveAsTextFile(sys.argv[2])
-
-os.remove("temp.txt")

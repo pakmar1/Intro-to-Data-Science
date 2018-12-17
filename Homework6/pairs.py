39ad255b30f8e8e345c47fbe0d027de1014ee386
Prasad Akmar <31735926+pakmar1@users.noreply.github.com>
2018-12-16 22:52:54 -0500

    final commit

--- Homework6/pairs.py
+++ Homework6/pairs.py
@@ -1,3 +1,4 @@
+from shutil import rmtree
 from pyspark import SparkContext
 import sys
 
@@ -5,12 +6,16 @@ sc = SparkContext("local", "app")
 
 graph_file = sc.textFile(sys.argv[1])
 
-counts = graph_file.map(lambda line: (line.split('\t')[0],line.split('\t')[1]))\
-.groupByKey(numPartitions=None)\
+counts = graph_file.map(lambda line: (line.split('\t')[0],line.split('\t')[1]))
+counts_rev = graph_file.map(lambda line: (line.split('\t')[1],line.split('\t')[0]))
+
+#intersection of the two rdds
+unwanted_rows = counts.subtractByKey(counts_rev)
+wanted_rows = counts.subtractByKey(unwanted_rows)
+
+wanted_rows = counts.groupByKey(numPartitions=None)\
 .mapValues(list)\
 .sortByKey(ascending=True)
 
-counts_1 = graph_file.map(lambda line: (line.split('\t')[1],line.split('\t')[0]))
-
-
-counts.saveAsTextFile(sys.argv[2])
+rmtree(sys.argv[2])
+wanted_rows.saveAsTextFile(sys.argv[2])
--- Homework6/weight.py
+++ Homework6/weight.py
@@ -1,3 +1,4 @@
+from shutil import rmtree
 from pyspark import SparkContext
 import sys
 
@@ -6,7 +7,8 @@ sc = SparkContext("local", "app")
 graph_file = sc.textFile(sys.argv[1])
 
 counts = graph_file.map(lambda degree: (degree.split('\t')[0],int(degree.split('\t')[2]))) \
-                   .reduceByKey(lambda a,b: a+b) \
-                   .sortByKey(ascending=True)
+.reduceByKey(lambda a,b: a+b) \
+.sortByKey(ascending=True)
 
+rmtree(sys.argv[2])
 counts.saveAsTextFile(sys.argv[2])
